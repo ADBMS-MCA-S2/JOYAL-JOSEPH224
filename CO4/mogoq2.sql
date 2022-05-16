75531dbc06c3c54a042d5d83fcb7ec9b8bb28f96
joyal-jkp <94290371+joyal-jkp@users.noreply.github.com>
2022-05-16 21:39:26 +0530

    Update mogoq2.sql

--- CO4/mogoq2.sql
+++ CO4/mogoq2.sql
@@ -14,3 +14,13 @@ WriteResult({ "nInserted" : 1 })
 > db.emp_details.insert({empid:105,empname:"remya",dept:"backend",desn:"tester",sal:24000,yoj:2009,addr:{dno:100,street:"barkroad",loc:"Marad",city:"Kochi"}});
 WriteResult({ "nInserted" : 1 })
 >
+ db.emp_details.find({ sal : { $gt :  50000, $lt : 75000}},{empname:1,sal:1,_id:0}).pretty();
+{ "empname" : "Derin", "sal" : 52000 }
+{ "empname" : "Athulya", "sal" : 51200 }
+
+> db.emp_details.find({desn:"developer"},{empname:1,desn:1,_id:0}).pretty();
+{ "empname" : "martin", "desn" : "developer" }
+{ "empname" : "Saliha", "desn" : "developer" }
+>
+> db.emp_details.find({empname:"rahul"},{empname:1,sal:1,_id:0});
+{ "empname" : "rahul", "sal" : 24000 }
