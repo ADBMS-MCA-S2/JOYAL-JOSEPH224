8ba73abb45e93fe413ee6aa0c9373b3d21e114c7
joyal-jkp <94290371+joyal-jkp@users.noreply.github.com>
2022-05-31 21:02:15 +0530

    Update MongoAssnosl.sql

--- CO4/MongoAssnosl.sql
+++ CO4/MongoAssnosl.sql
@@ -31,3 +31,35 @@ db.studlist.find({gender:"male",grade:"A+"},{name:1,_id:0}).pretty();
 { "name" : { "fname" : "partha", "lname" : "sarthi" } }
 { "name" : { "fname" : "jane", "lname" : "dine" } }
 { "name" : { "fname" : "marp", "lname" : "terpan" } }
+
+
+db.studlist.find({mark:{$gt:90},gender:'female'},{name:1,mark:1,contact:1,grade:1,_id:0})
+{ "name" : { "fname" : "Thara", "lname" : "palis" }, "mark" : 92, "grade" : "A+", "contact" : 932334242 }
+{ "name" : { "fname" : "partha", "lname" : "sarthi" }, "mark" : 94, "grade" : "A", "contact" : 944705321 }
+
+
+
+ db.studlist.find({$and:[{mark:{$gt:80}},{mark:{$lt:90}}]},{name:1,contact:1,grade:1,_id:0})
+{ "name" : { "fname" : "werner", "lname" : "sali" }, "grade" : "A", "contact" : 932333242 }
+{ "name" : { "fname" : "guruppa", "lname" : "maliya" }, "grade" : "B+", "contact" : 944788781 }
+
+
+
+
+
+
+
+ db.studlist.find({loc:"kollam"},{name:1,_id:0})
+{ "name" : { "fname" : "martin", "lname" : "luthor" } }
+{ "name" : { "fname" : "partha", "lname" : "sarthi" } }
+
+
+db.studlist.find({$nor:[{loc:"kollam"},{loc:"thiruvananthapuram"}]},{name:1,loc:1,_id:0})
+{ "name" : { "fname" : "Thara", "lname" : "palis" }, "loc" : "trissur" }
+{ "name" : { "fname" : "marp", "lname" : "terpan" }, "loc" : "Ernakulam" }
+{ "name" : { "fname" : "werner", "lname" : "sali" }, "loc" : "kochi" }
+{ "name" : { "fname" : "guruppa", "lname" : "maliya" }, "loc" : "palakad" }
+
+
+ db.studlist.find({$or:[{loc:"kollam"},{loc:"thiruvananthapuram"}],gender:"female"},{name:1,loc:1,gender:1,_id:0})
+{ "name" : { "fname" : "partha", "lname" : "sarthi" }, "loc" : "kollam", "gender" : "female" }
