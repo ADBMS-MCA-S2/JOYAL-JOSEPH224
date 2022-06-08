7af69d67bfa8c91760f8828ed7257b112bc31f1f
joyal-jkp <94290371+joyal-jkp@users.noreply.github.com>
2022-06-08 15:33:19 +0530

    Update indexes.sql

--- co7/indexes.sql
+++ co7/indexes.sql
@@ -16,21 +16,10 @@ WriteResult({ "nInserted" : 1 })
         }
 }
 > db.register.insert({name:"joyal",regno:121,mark:[maths:87,english:87,computer:99]})
-uncaught exception: SyntaxError: missing ] after element list :
-@(shell):1:54
 > db.register.insert({name:"joyal",regno:121,mark:[{maths:87,english:87,computer:99}]})
 WriteResult({ "nInserted" : 1 })
 > db.register.find().pretty();
-{
-        "_id" : ObjectId("62a06b8b6e2fc1288e065ad6"),
-        "name" : "joyal",
-        "regno" : 121,
-        "mark" : {
-                "maths" : 87,
-                "english" : 87,
-                "computer" : 99
-        }
-}
+
 {
         "_id" : ObjectId("62a06c766e2fc1288e065ad7"),
         "name" : "joyal",
@@ -43,6 +32,7 @@ WriteResult({ "nInserted" : 1 })
                 }
         ]
 }
+
 > db.register.insert({name:"marthandan",regno:102,mark:[{maths:54,english:67,computer:95}]})
 WriteResult({ "nInserted" : 1 })
 > db.register.insert({name:"alex",regno:103,mark:[{maths:54,english:67,computer:95}]})
@@ -52,14 +42,11 @@ WriteResult({ "nInserted" : 1 })
 > db.register.insert({name:"karthi",regno:123,mark:[{maths:67,english:78,computer:46}]})
 WriteResult({ "nInserted" : 1 })
 > db.register.craeteIndex(regno:1)
-uncaught exception: SyntaxError: missing ) after argument list :
-@(shell):1:29
+
 > db.register.createIndex(regno:1)
-uncaught exception: SyntaxError: missing ) after argument list :
-@(shell):1:29
+
 > db.register.createIndex(r{egno:1})
-uncaught exception: SyntaxError: missing ) after argument list :
-@(shell):1:25
+
 > db.register.createIndex({regno:1})
 {
         "numIndexesBefore" : 1,
@@ -67,21 +54,8 @@ uncaught exception: SyntaxError: missing ) after argument list :
         "createdCollectionAutomatically" : false,
         "ok" : 1
 }
-> db.register.getIndexes
-function(filter) {
-    var res = this.runCommand("listIndexes", filter);
 
-    if (!res.ok) {
-        if (res.code == ErrorCodes.NamespaceNotFound) {
-            // For compatibility, return []
-            return [];
-        }
 
-        throw _getErrorWithCode(res, "listIndexes failed: " + tojson(res));
-    }
-
-    return new DBCommandCursor(this._db, res).toArray();
-}
 > db.register.getIndexes()
 [
         {
@@ -111,21 +85,7 @@ SINGLE FIELD INDEX
         "createdCollectionAutomatically" : false,
         "ok" : 1
 }
-> db.register.getIndexes
-function(filter) {
-    var res = this.runCommand("listIndexes", filter);
 
-    if (!res.ok) {
-        if (res.code == ErrorCodes.NamespaceNotFound) {
-            // For compatibility, return []
-            return [];
-        }
-
-        throw _getErrorWithCode(res, "listIndexes failed: " + tojson(res));
-    }
-
-    return new DBCommandCursor(this._db, res).toArray();
-}
 > db.register.getIndexes()
 [
         {
@@ -144,8 +104,7 @@ function(filter) {
         }
 ]
 > db.dropIndex({regno:1})
-uncaught exception: TypeError: db.dropIndex is not a function :
-@(shell):1:1
+
 > db.register.dropIndex({regno:1})
 { "nIndexesWas" : 2, "ok" : 1 }
 > db.register.getIndexes()
@@ -231,3 +190,8 @@ db.register.createIndex({"mark.maths":1})
                 "name" : "mark.english_1"
         }
 ]
+
+
+
+TEXT INDEXES
+
